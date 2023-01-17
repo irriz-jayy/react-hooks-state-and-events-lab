f2f1a9a234b58629c58986ecd2b5f3f10f54c4ad
irriz-jayy <jayfelix789@gmail.com>
2023-01-17 19:02:29 +0300

    Added cart functionality

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [cart, inCart] = useState(false);
+
+  function handleClick() {
+    inCart((cart) => !cart);
+  }
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">
+        {cart ? "Remove from cart" : "Add to cart"}
+      </button>
     </li>
   );
 }
